4056a99228e0632522120454f5e5235b223a096e
Rasika-Deodhar <rasikadeodhar95@gmail.com>
2017-08-10 17:48:02 +0530

    Message

---
+++ C#/Assignment3/Assignment3/AreaMasterExtension.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class CountryMaster:AreaMaster
+    {
+        public CountryMaster() { }
+
+        public CountryMaster(int ID, string Code, int AreaID, string Name, string Description, AreaType Area) :base(ID,Code,AreaID,Name,Description,Area)
+        {
+
+        }
+
+        public override AreaType Area
+        {
+            get
+            {
+                return AreaType.Country;
+            }
+
+            set
+            {
+                base.Area = AreaType.Country;
+            }
+        }
+        
+    }
+
+    class StateMaster : AreaMaster
+    {
+        public StateMaster()
+        { }
+
+        public StateMaster(int ID, string Code, int AreaID, string Name, string Description, AreaType Area) :base(ID,Code,AreaID,Name,Description,Area)
+        {
+
+        }
+
+        public override AreaType Area
+        {
+            get
+            {
+                return AreaType.State;
+            }
+
+            set
+            {
+                base.Area = AreaType.State;
+            }
+        }
+    }
+
+    class CityMaster : AreaMaster
+    {
+        public CityMaster()
+        { }
+
+        public CityMaster(int ID, string Code, int AreaID, string Name, string Description, AreaType Area) :base(ID,Code,AreaID,Name,Description,Area)
+        {
+
+        }
+
+        public override AreaType Area
+        {
+            get
+            {
+                return AreaType.City;
+            }
+
+            set
+            {
+                base.Area = AreaType.City;
+            }
+        }
+        
+    }
+
+    class DistrictMaster : AreaMaster
+    {
+        public DistrictMaster()
+        { }
+
+        public DistrictMaster(int ID, string Code, int AreaID, string Name, string Description, AreaType Area) :base(ID,Code,AreaID,Name,Description,Area)
+        {
+
+        }
+
+        public override AreaType Area
+        {
+            get
+            {
+                return AreaType.District;
+            }
+
+            set
+            {
+                base.Area = AreaType.District;
+            }
+        }
+
+    }
+}
---
+++ C#/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AreaMaster am1 = new CountryMaster(1,"IND",0,"India","Anything",AreaMaster.AreaType.Country);
+            AreaMaster am2 = new StateMaster(2, "MH", 1, "Maharashtra", "Anything",AreaMaster.AreaType.State);
+            AreaMaster am3 = new CityMaster(3, "PUN", 2, "Pune", "Anything",AreaMaster.AreaType.City);
+            AreaMaster am4 = new DistrictMaster(4, "PD", 3, "Pune District", "Anything",AreaMaster.AreaType.District);
+            AreaMaster am = new AreaMaster();
+
+            foreach (var item in AreaMaster.AreaCollection)
+            {
+                Console.WriteLine("ID : " + item.ID);
+                Console.WriteLine("Code : " + item.Code);
+                Console.WriteLine("Area ID : " + item.AreaID);
+                Console.WriteLine("Name : " + item.Name);
+                Console.WriteLine("Description : " + item.Description);
+                Console.WriteLine("Area Type : " + item.Area);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+            am.GetAreaMasterByCity("Pune");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
