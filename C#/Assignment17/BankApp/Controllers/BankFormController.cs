d75e1f9043a1424f052e0b497c891bc0c5e15b64
Rasika-Deodhar <rasikadeodhar95@gmail.com>
2017-09-04 17:44:35 +0530

    mess

---
+++ C#/Assignment17/BankApp/Controllers/BankFormController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankApp.Controllers
+{
+    public class BankFormController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: BankForm
+        
+        public ActionResult Submit()
+        {            
+            return View();
+        }
+
+        public ActionResult AfterLogin(Bank bank)
+        {
+            using (BankDataEntities bde = new BankDataEntities())
+            {
+                bde.Banks.Add(bank);
+                bde.SaveChanges();
+            }
+                return View(bank);
+        }
+
+    }
+}
---
+++ C#/Assignment17/BankApp/Models/BankClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BankApp.Models
+{
+    public class BankClass
+    {
+        [Key]
+        
+        public string AccountNumber { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string AccountType { get; set; }
+    }
+}
